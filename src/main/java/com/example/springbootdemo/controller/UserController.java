0394af7c5bab6bcea53d30c2fba1cd9bf0bb2a0d
Yha2682 <yha310776@gmail.com>
2025-04-04 23:53:01 +0800

    更新了用户登录接口

--- src/main/java/com/example/springbootdemo/controller/UserController.java
+++ src/main/java/com/example/springbootdemo/controller/UserController.java
@@ -4,9 +4,11 @@ package com.example.springbootdemo.controller;
 import com.example.springbootdemo.pojo.Result;
 import com.example.springbootdemo.pojo.User;
 import com.example.springbootdemo.service.UserService;
+import com.example.springbootdemo.utils.Md5Util;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +32,21 @@ public class UserController {
             //被占用了，注册失败
             return Result.error("注册失败，用户名被占用!");
         }
+    }
+
+    //用户登录
+    @PostMapping("/login")
+    public Result login(@Pattern(regexp = "^\\s{5,16}$") String username, @Pattern(regexp = "^\\s{5,16}$")  String password) {
+        //查找用户名
+        User user = userService.findByUserName(username);
+        //判断是否存在
+        if (user == null) {
+            return Result.error("用户名错误!");
+        }
+        if (Md5Util.getMD5String(password).equals(user.getPassword())) {
+            //登录成功
+            return Result.success("jwt token令牌...");
+        }
+        return Result.error("密码错误！");
     }
 }
