4109ae8c529bc5daf85bbecde7c78c146a2a2d53
Yha2682 <yha310776@gmail.com>
2025-04-09 23:03:06 +0800

    最终版

--- src/main/java/com/example/springbootdemo/controller/FileUploadController.java
+++ src/main/java/com/example/springbootdemo/controller/FileUploadController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.UUID;
 
 @RestController
